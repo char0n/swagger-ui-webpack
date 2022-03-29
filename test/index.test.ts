cf2db6829ce4f211465b0278b6f33f1128f41dd2
Scott O'Hara <scott@oharagroup.net>
2022-03-29 18:53:34 +1100

    Add ts-jest

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,11 @@ import SwaggerUI from "swagger-ui";
 
 describe("swagger-ui", () => {
 	it("should work", () => {
+		SwaggerUI({
+			dom_id: "#app",
+			url: "https://petstore.swagger.io/v2/swagger.json"
+		});
+		
 		expect(true).toBe(true);
 	});
 });
