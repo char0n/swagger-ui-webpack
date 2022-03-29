903b61f959dbc881011fb6216f9f0eea0d404c97
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-03-29 11:04:18 +0200

    working swagger-ui test

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,6 @@ import SwaggerUI from "swagger-ui";
 describe("swagger-ui", () => {
 	it("should work", () => {
 		SwaggerUI({
-			dom_id: "#app",
 			url: "https://petstore.swagger.io/v2/swagger.json"
 		});
 		
