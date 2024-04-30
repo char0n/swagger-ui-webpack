767f8c0a284075c3f60cf6ff859ee2139739d155
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-04-30 09:44:05 +0200

    chore: add support for React@18

--- src/swagger-ui-react.js
+++ src/swagger-ui-react.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import SwaggerUI from 'swagger-ui-react';
 
 const props = { url: 'https://petstore3.swagger.io/api/v3/openapi.json' };
 const element = React.createElement(SwaggerUI, props);
 const domContainer = document.querySelector('#swagger-ui-react');
+const root = createRoot(domContainer);
 
-ReactDOM.render(element, domContainer);
+root.render(element);
